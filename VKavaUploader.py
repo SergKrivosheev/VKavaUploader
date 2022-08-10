0e46659a5c9a955134a0ba52f933b045549356a1
SergKrivosheev <krivosheev.sergey89@gmail.com>
2022-08-10 13:24:28 +0300

    Second commit with changes after review

--- VKavaUploader.py
+++ VKavaUploader.py
@@ -1,17 +1,62 @@
 import requests
-from pprint import pprint
 from tqdm import tqdm
 import json
 import time
+import configparser
+from pprint import pprint
 
-class VKavaUploader:
-    url_vk = 'https://api.vk.com/method/'
-    url_ya = 'https://cloud-api.yandex.net/v1/disk/resources/'
-    def __init__(self, VKTOKEN, YATOKEN):
+class VKDownloader:
+    VKURL = 'https://api.vk.com/method/'
+    def __init__(self, VKTOKEN):
         self.params = {
             'access_token': VKTOKEN,
             'v': '5.131'
         }
+
+    def get_user_id(self, id):
+        get_user_id_url = self.VKURL + 'users.get'
+        get_user_id_params = {
+            'users_id': id
+        }
+        req = requests.get(get_user_id_url, params={**self.params, **get_user_id_params}).json()
+        for item in req["response"]:
+            user_id = item['id']
+        time.sleep(1)
+        return user_id
+
+    def get_photos(self, id, photos_quantity):
+        get_photos_url = self.VKURL + 'photos.get'
+        if id.isdigit():
+            user_id = id
+        else:
+            user_id = self.get_user_id(id)
+        get_photos_params = {
+            'owner_id': user_id,
+            'album_id': 'profile',
+            'extended': 1,
+            'count': photos_quantity
+        }
+        req = requests.get(get_photos_url, params={**self.params, **get_photos_params}).json()
+        dict_photos = {}
+        for item in tqdm(req['response']['items']):
+            photo_name = item['likes']['count']
+            for photo in item['sizes']:
+                if photo['type'] == 'z' or photo['type'] == 'w' or photo['type'] == 'y' or photo['type'] == 'x': #т.к. у меня всего одна аватарка с размером z поэтому я выбираю просто все большие
+                    if photo_name not in dict_photos:
+                        dict_photos[photo_name] = [photo['url'], photo['type'], item['date']]
+                        print(dict_photos)
+                        break
+                    else:
+                        photo_name = f'{photo_name}_{str(item["date"])}'
+                        dict_photos[photo_name] = [photo['url'], photo['type'], item['date']]
+                        print(dict_photos)
+                        break
+            time.sleep(0.33)
+        return dict_photos # возвращаем как результат словарь с key - количество лайков, value - 1 ссылка, 2 размер
+
+class YaUploader:
+    YAURL = 'https://cloud-api.yandex.net/v1/disk/resources/'
+    def __init__(self, YATOKEN):
         self.yatoken = YATOKEN
 
     def get_headers(self):
@@ -20,73 +65,54 @@ class VKavaUploader:
             'Authorization': 'OAuth {}'.format(self.yatoken)
         }
 
-    def _get_upload_link(self, file_path):
-        upload_url = self.url_ya + "upload"
+    def get_upload_link(self, file_path):
+        upload_url = self.YAURL + "upload"
         headers = self.get_headers()
         params = {"path": file_path, "overwrite": "true"}
         response = requests.get(upload_url, headers=headers, params=params)
         pprint(response.json())
         return response.json()
 
-    def photos_ava_get(self):
-        photos_ava_get_url = self.url_vk + 'photos.get'
-        photos_ava_get_params = {
-            'owner_id': '33258642',
-            'album_id': 'profile',
-            'extended': 1
-        }
-        req = requests.get(photos_ava_get_url, params={**self.params, **photos_ava_get_params}).json()
-        dict_photos = {}
-        for item in tqdm(req['response']['items']):
-            dict_photos[item['likes']['count']] = ''
-            for item_ in item['sizes']:
-                if item_['type'] == 'z' or item_['type'] == 'w' or item_['type'] == 'y' or item_['type'] == 'x': #т.к. у меня всего одна аватарка с размером z поэтому я выбираю просто все большие
-                    dict_photos[item['likes']['count']] = [item_['url'], item_['type']]
-                    break
-            time.sleep(0.33)
-        return dict_photos #возвращаем как результат словарь с key - количество лайков, value - 1 ссылка, 2 размер
+    def upload_json(self, dictionary, file_path):
+        for item in tqdm(dictionary):
+            json_file = [{"file name": f"{item}.jpg", "size": f"{dictionary[item][1]}"}]
+            with open(f'{item}.json', 'w') as outfile:
+                json.dump(json_file, outfile)
+            href = self.get_upload_link(file_path=f'{file_path}/{item}.json').get("href", "")
+            response = requests.put(href, data=open(f'{item}.json', 'rb'))
+            response.raise_for_status()
+            if response.status_code == 201:
+                print("Success")
 
-    def photos_upload(self):
-        photos = self.photos_ava_get()
-        counter = 0
-        for item in tqdm(photos):
-            if counter < 5:
-                r = requests.get(photos[item][0])
-                href = self._get_upload_link(file_path=f'netology/{item}.jpg').get("href", "")
-                response = requests.put(href, data=r.content)
-                response.raise_for_status()
-                if response.status_code == 201:
-                    print("Success")
-                time.sleep(0.33)
-                counter += 1
-            else:
-                break
-        self.json_upload(photos) #выполняем загрузку json файлов, как аргумент используем словарь
+    def upload_photos(self, dictionary, file_path):
+        self.create_directory(file_path)
+        for item in tqdm(dictionary):
+            r = requests.get(dictionary[item][0])
+            href = self.get_upload_link(file_path=f'{file_path}/{item}.jpg').get("href", "")
+            response = requests.put(href, data=r.content)
+            response.raise_for_status()
+            if response.status_code == 201:
+                print("Success")
+                time.sleep(1)
+        self.upload_json(dictionary, file_path)  # выполняем загрузку json файлов, как аргумент используем словарь
 
-    def json_upload(self, dictionary):
-        dict_with_info = dictionary
-        counter = 0
-        for item in tqdm(dict_with_info):
-            if counter < 5:
-                json_file = [{"file name": f"{item}.jpg", "size": f"{dict_with_info[item][1]}"}]
-                with open(f'{item}.json', 'w') as outfile:
-                    json.dump(json_file, outfile)
-                href = self._get_upload_link(file_path=f'netology/{item}.json').get("href", "")
-                response = requests.put(href, data=open(f'{item}.json', 'rb'))
-                response.raise_for_status()
-                if response.status_code == 201:
-                    print("Success")
-                counter += 1
-            else:
-                break
+    def create_directory(self, path):
+        requests.put(f'{self.YAURL}?path={path}', headers=self.get_headers())
 
 if __name__ == '__main__':
 
-    with open('YaToken.txt', 'r') as f:
-        YATOKEN = f.read()
+    #with open('tokens.txt', 'r') as f: # простой способ чтения токенов из одного файла
+        #VKTOKEN = f.readline()
+        #YATOKEN = f.readline()
 
-    with open('VKToken.txt', 'r') as f:
-        VKTOKEN = f.read()
+    config = configparser.ConfigParser()  # через .ini файл
+    config.read("tokens.ini")
 
-    client = VKavaUploader(VKTOKEN, YATOKEN)
-    client.photos_upload()
+    photos_quantity = input('Сколько фотографий вы бы хотели копировать?')
+    vk_user_id = input('Введите user id или screen name')
+    dir_name = input('В какую папку хотите положить фотографии?')
+    VKTOKEN = config['VK']['token'].strip('"')
+    YATOKEN = config['Yandex']['token'].strip('"')
+    downloader = VKDownloader(VKTOKEN)
+    uploader = YaUploader(YATOKEN)
+    uploader.upload_photos(downloader.get_photos(vk_user_id, int(photos_quantity)), dir_name)
